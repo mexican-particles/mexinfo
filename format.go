906196e7fbf0ecfc7a68166d17a687729b8e5712
ya310 <mentol310@gmail.com>
2020-02-13 23:35:26 +0900

    link として認識して

--- format.go
+++ format.go
@@ -3,7 +3,7 @@ package mexinfo
 func formatSlackMessage(link string) (*Message, error) {
 	message := &Message{
 		ResponseType: "in_channel",
-		Text:         link,
+		Text:         "<" + link + ">",
 		UnfurlLinks:  true,
 	}
 	return message, nil
